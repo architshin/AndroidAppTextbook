0610871bdd501504c2e104c395a77c8817f233cf
Shinzo SAITO <architshin@websarva.com>
2017-06-21 01:39:31 +0900

    第5章「リスト選択サンプル」の追加。

---
+++ ListViewSample/app/src/main/java/com/websarva/wings/android/listviewsample/ListViewSampleActivity.java
@@ -0,0 +1,42 @@
+package com.websarva.wings.android.listviewsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+/**
+ * 『Androidアプリ開発の教科書』
+ * 第5章
+ * リスト選択サンプル
+ *
+ * メインアクティビティクラス。
+ *
+ * @author Shinzo SAITO
+ */
+public class ListViewSampleActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_list_view_sample);
+
+		ListView lvMenu = (ListView) findViewById(R.id.lvMenu);
+		lvMenu.setOnItemClickListener(new ListItemClickListener());
+	}
+
+	/**
+	 * リストが選択されたときの処理が記述されたメンバクラス。
+	 */
+	private class ListItemClickListener implements AdapterView.OnItemClickListener {
+
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			String item = (String) parent.getItemAtPosition(position);
+			String show = "あなたが選んだ定食: " + item;
+			Toast.makeText(ListViewSampleActivity.this, show, Toast.LENGTH_LONG).show();
+		}
+	}
+}
