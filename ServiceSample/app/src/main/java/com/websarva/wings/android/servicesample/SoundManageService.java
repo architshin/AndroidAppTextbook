473c38298844ac2279d0224241bb83a3c10e2a29
Shinzo SAITO <architshin@websarva.com>
2017-08-08 18:19:13 +0900

    サービスに関するコードを記述。

---
+++ ServiceSample/app/src/main/java/com/websarva/wings/android/servicesample/SoundManageService.java
@@ -0,0 +1,101 @@
+package com.websarva.wings.android.servicesample;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.io.IOException;
+
+/**
+ * 『Androidアプリ開発の教科書』
+ * 第13章
+ * サービスサンプル
+ *
+ * サービスクラス。
+ *
+ * @author Shinzo SAITO
+ */
+public class SoundManageService extends Service {
+
+	/**
+	 * メディアプレーヤーフィールド。
+	 */
+	private MediaPlayer _player;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		//フィールドのメディアプレーヤーオブジェクトを生成。
+		_player = new MediaPlayer();
+	}
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		//音声ファイルのURI文字列を作成。
+		String mediaFileUriStr = "android.resource://" + getPackageName() + "/" + R.raw.mountain_stream;
+		//音声ファイルのURI文字列を元にURIオブジェクトを生成。
+		Uri mediaFileUri = Uri.parse(mediaFileUriStr);
+		try {
+			//メディアプレーヤーに音声ファイルを指定。
+			_player.setDataSource(SoundManageService.this, mediaFileUri);
+			//非同期でのメディア再生準備が完了した際のリスナを設定。
+			_player.setOnPreparedListener(new PlayerPreparedListener());
+			//メディア再生が終了した際のリスナを設定。
+			_player.setOnCompletionListener(new PlayerCompletionListener());
+			//非同期でメディア再生を準備。
+			_player.prepareAsync();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		//親クラスのメソッドを呼び出し、その戻り値をそのままリターン。
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+
+	@Override
+	public void onDestroy() {
+		//親クラスのメソッド呼び出し。
+		super.onDestroy();
+		//プレーヤーが再生中なら…
+		if(_player.isPlaying()) {
+			//プレーヤーを停止。
+			_player.stop();
+		}
+		//プレーヤーを解放。
+		_player.release();
+		//プレーヤー用フィールドをnullに。
+		_player = null;
+	}
+
+	/**
+	 * メディア再生準備が完了時のリスナクラス。
+	 */
+	private class PlayerPreparedListener implements MediaPlayer.OnPreparedListener {
+
+		@Override
+		public void onPrepared(MediaPlayer mp) {
+			//メディアを再生。
+			mp.start();
+		}
+	}
+
+	/**
+	 * メディア再生が終了したときのリスナクラス。
+	 */
+	private class PlayerCompletionListener implements MediaPlayer.OnCompletionListener {
+
+		@Override
+		public void onCompletion(MediaPlayer mp) {
+			//自分自身を終了。
+			stopSelf();
+		}
+	}
+}
--- ServiceSample/app/src/main/java/com/websarva/wings/android/servicesample/SoundStartActivity.java
+++ ServiceSample/app/src/main/java/com/websarva/wings/android/servicesample/SoundStartActivity.java
@@ -1,13 +1,59 @@
 package com.websarva.wings.android.servicesample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+/**
+ * 『Androidアプリ開発の教科書』
+ * 第13章
+ * サービスサンプル
+ *
+ * アクティビティクラス。
+ *
+ * @author Shinzo SAITO
+ */
 public class SoundStartActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sound_start);
+	}
+
+	/**
+	 * 再生ボタンタップ時の処理メソッド。
+	 *
+	 * @param view 画面部品
+	 */
+	public void onPlayButtonClick(View view) {
+		//インテントオブジェクトを生成。
+		Intent intent = new Intent(SoundStartActivity.this, SoundManageService.class);
+		//サービスを起動。
+		startService(intent);
+		//再生ボタンをタップ不可に、停止ボタンをタップ可に変更。
+		Button btPlay = (Button) findViewById(R.id.btPlay);
+		Button btStop = (Button) findViewById(R.id.btStop);
+		btPlay.setEnabled(false);
+		btStop.setEnabled(true);
+	}
+
+	/**
+	 * 停止ボタンタップ時の処理メソッド。
+	 *
+	 * @param view 画面部品
+	 */
+	public void onStopButtonClick(View view) {
+		//インテントオブジェクトを生成。
+		Intent intent = new Intent(SoundStartActivity.this, SoundManageService.class);
+		//サービスを停止。
+		stopService(intent);
+		//再生ボタンをタップ可に、停止ボタンをタップ不可に変更。
+		Button btPlay = (Button) findViewById(R.id.btPlay);
+		Button btStop = (Button) findViewById(R.id.btStop);
+		btPlay.setEnabled(true);
+		btStop.setEnabled(false);
 	}
 }
