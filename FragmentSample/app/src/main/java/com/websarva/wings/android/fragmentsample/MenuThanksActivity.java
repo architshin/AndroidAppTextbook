4cf584970e3346076c0477f3931fb42cc338a485
Shinzo SAITO <architshin@websarva.com>
2017-10-04 01:25:22 +0900

    「第9章 フラグメントサンプル」を本脱稿に合わせて書き換え。

--- FragmentSample/app/src/main/java/com/websarva/wings/android/fragmentsample/MenuThanksActivity.java
+++ FragmentSample/app/src/main/java/com/websarva/wings/android/fragmentsample/MenuThanksActivity.java
@@ -3,6 +3,15 @@ package com.websarva.wings.android.fragmentsample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 『Androidアプリ開発の教科書』
+ * 第9章
+ * フラグメントサンプル
+ *
+ * 注文完了画面のアクティビティクラス。
+ *
+ * @author Shinzo SAITO
+ */
 public class MenuThanksActivity extends AppCompatActivity {
 
 	@Override
