0e1b29b312033fd2d7d6f2356859c1dcc0c4b960
Shinzo SAITO <architshin@websarva.com>
2017-10-17 16:48:07 +0900

    完成。

--- CameraIntentSample/app/src/main/java/com/websarva/wings/android/cameraintentsample/UseCameraActivity.java
+++ CameraIntentSample/app/src/main/java/com/websarva/wings/android/cameraintentsample/UseCameraActivity.java
@@ -1,12 +1,21 @@
 package com.websarva.wings.android.cameraintentsample;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * 『Androidアプリ開発の教科書』
@@ -18,6 +27,10 @@ import android.widget.ImageView;
  * @author Shinzo SAITO
  */
 public class UseCameraActivity extends AppCompatActivity {
+	/**
+	 * 保存された画像のURI。
+	 */
+	private Uri _imageUri;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,14 +40,22 @@ public class UseCameraActivity extends AppCompatActivity {
 
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		//カメラアプリとの連携からの戻りでかつ撮影成功の場合
+		//カメラアプリからの戻りでかつ撮影成功の場合
 		if(requestCode == 200 && resultCode == RESULT_OK) {
-			//撮影された画像のビットマップデータを取得。
-			Bitmap bitmap =  data.getParcelableExtra("data");
 			//画像を表示するImageViewを取得。
 			ImageView ivCamera = (ImageView) findViewById(R.id.ivCamera);
-			//撮影された画像をImageViewに設定。
-			ivCamera.setImageBitmap(bitmap);
+			//フィールドの画像URIをImageViewに設定。
+			ivCamera.setImageURI(_imageUri);
+		}
+	}
+
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		//WRITE_EXTERNAL_STORAGEに対するパーミションダイアログでかつ許可を選択したなら…
+		if(requestCode == 2000 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+			//もう一度カメラアプリを起動。
+			ImageView ivCamera = (ImageView) findViewById(R.id.ivCamera);
+			onCameraImageClick(ivCamera);
 		}
 	}
 
@@ -44,8 +65,38 @@ public class UseCameraActivity extends AppCompatActivity {
 	 * @param view 画面部品
 	 */
 	public void onCameraImageClick(View view) {
+		//WRITE_EXTERNAL_STORAGEの許可が下りていないなら…
+		if(ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+			//WRITE_EXTERNAL_STORAGEの許可を求めるダイアログを表示。その際、リクエストコードを2000に設定。
+			String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+			ActivityCompat.requestPermissions(this, permissions, 2000);
+			return;
+		}
+
+		//日時データを「yyyyMMddHHmmss」の形式に整形するフォーマッタを生成。
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+		//現在の日時を取得。
+		Date now = new Date(System.currentTimeMillis());
+		//取得した日時データを「yyyyMMddHHmmss」形式に整形した文字列を生成。
+		String nowStr = dateFormat.format(now);
+		//ストレージに格納する画像のファイル名を生成。ファイル名の一意を確保するためにタイムスタンプの値を利用。
+		String fileName = "UseCameraActivityPhoto_" + nowStr +".jpg";
+
+		//ContentValuesオブジェクトを生成。
+		ContentValues values = new ContentValues();
+		//画像ファイル名を設定。
+		values.put(MediaStore.Images.Media.TITLE, fileName);
+		//画像ファイルの種類を設定。
+		values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+
+		//ContentResolverオブジェクトを生成。
+		ContentResolver resolver = getContentResolver();
+		//ContentResolverを使ってURIオブジェクトを生成。
+		_imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
 		//Intentオブジェクトを生成。
 		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		//Extra情報として_imageUriを設定。
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, _imageUri);
 		//アクティビティを起動。
 		startActivityForResult(intent, 200);
 	}
