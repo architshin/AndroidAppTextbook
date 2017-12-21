76bbf6cdd10ef5fe4c8fde1b04e44e48c7da22e2
Shinzo SAITO <architshin@websarva.com>
2017-12-21 14:35:52 +0900

    第8章「メニューサンプル」を初校に合わせて変更。

--- MenuSample/app/src/main/java/com/websarva/wings/android/menusample/MenuListActivity.java
+++ MenuSample/app/src/main/java/com/websarva/wings/android/menusample/MenuListActivity.java
@@ -55,7 +55,7 @@ public class MenuListActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_menu_list);
 
 		//画面部品ListViewを取得し、フィールドに格納。
-		_lvMenu = (ListView) findViewById(R.id.lvMenu);
+		_lvMenu = findViewById(R.id.lvMenu);
 
 		//SimpleAdapterで使用する定食メニューListオブジェクトをprivateメソッドを利用して用意し、フィールドに格納。
 		_menuList = createTeishokuList();
@@ -155,72 +155,72 @@ public class MenuListActivity extends AppCompatActivity {
 	 */
 	private List<Map<String, Object>> createTeishokuList() {
 		//定食メニューリスト用のListオブジェクトを用意。
-		List<Map<String, Object>> menuList = new ArrayList<Map<String,Object>>();
+		List<Map<String, Object>> menuList = new ArrayList<>();
 
 		//「から揚げ定食」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
-		Map<String, Object> menu = new HashMap<String, Object>();
+		Map<String, Object> menu = new HashMap<>();
 		menu.put("name", "から揚げ定食");
 		menu.put("price", 800);
 		menu.put("desc", "若鳥のから揚げにサラダ、ご飯とお味噌汁が付きます。");
 		menuList.add(menu);
 
 		//「ハンバーグ定食」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
-		menu = new HashMap<String, Object>();
+		menu = new HashMap<>();
 		menu.put("name", "ハンバーグ定食");
 		menu.put("price", 850);
 		menu.put("desc", "手ごねハンバーグにサラダ、ご飯とお味噌汁が付きます。");
 		menuList.add(menu);
 
 		//以下データ登録の繰り返し。
-		menu = new HashMap<String, Object>();
+		menu = new HashMap<>();
 		menu.put("name", "生姜焼き定食");
 		menu.put("price", 850);
 		menu.put("desc", "すりおろし生姜を使った生姜焼きにサラダ、ご飯とお味噌汁が付きます。");
 		menuList.add(menu);
 
-		menu = new HashMap<String, Object>();
+		menu = new HashMap<>();
 		menu.put("name", "ステーキ定食");
 		menu.put("price", 1000);
 		menu.put("desc", "国産牛のステーキにサラダ、ご飯とお味噌汁が付きます。");
 		menuList.add(menu);
 
-		menu = new HashMap<String, Object>();
+		menu = new HashMap<>();
 		menu.put("name", "野菜炒め定食");
 		menu.put("price", 750);
 		menu.put("desc", "季節の野菜炒めにサラダ、ご飯とお味噌汁が付きます。");
 		menuList.add(menu);
 
-		menu = new HashMap<String, Object>();
+		menu = new HashMap<>();
 		menu.put("name", "とんかつ定食");
 		menu.put("price", 900);
 		menu.put("desc", "ロースとんかつにサラダ、ご飯とお味噌汁が付きます。");
 		menuList.add(menu);
 
-		menu = new HashMap<String, Object>();
+		menu = new HashMap<>();
 		menu.put("name", "ミンチかつ定食");
 		menu.put("price", 850);
 		menu.put("desc", "手ごねミンチカツにサラダ、ご飯とお味噌汁が付きます。");
 		menuList.add(menu);
 
-		menu = new HashMap<String, Object>();
+		menu = new HashMap<>();
 		menu.put("name", "チキンカツ定食");
 		menu.put("price", 900);
 		menu.put("desc", "ボリュームたっぷりチキンカツにサラダ、ご飯とお味噌汁が付きます。");
 		menuList.add(menu);
 
-		menu = new HashMap<String, Object>();
+		menu = new HashMap<>();
 		menu.put("name", "コロッケ定食");
 		menu.put("price", 850);
 		menu.put("desc", "北海道ポテトコロッケにサラダ、ご飯とお味噌汁が付きます。");
 		menuList.add(menu);
 
-		menu = new HashMap<String, Object>();
+		menu = new HashMap<>();
 		menu.put("name", "焼き魚定食");
 		menu.put("price", 850);
 		menu.put("desc", "鰆の塩焼きにサラダ、ご飯とお味噌汁が付きます。");
 		menuList.add(menu);
 
-		menu = new HashMap<String, Object>();
+		menu = new HashMap<>();
 		menu.put("name", "焼肉定食");
 		menu.put("price", 950);
 		menu.put("desc", "特性たれの焼肉にサラダ、ご飯とお味噌汁が付きます。");
@@ -236,48 +236,48 @@ public class MenuListActivity extends AppCompatActivity {
 	 */
 	private List<Map<String, Object>> createCurryList() {
 		//カレーメニューリスト用のListオブジェクトを用意。
-		List<Map<String, Object>> menuList = new ArrayList<Map<String,Object>>();
+		List<Map<String, Object>> menuList = new ArrayList<>();
 
 		//「ビーフカレー」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
-		Map<String, Object> menu = new HashMap<String, Object>();
+		Map<String, Object> menu = new HashMap<>();
 		menu.put("name", "ビーフカレー");
 		menu.put("price", 520);
 		menu.put("desc", "特選スパイスをきかせた国産ビーフ100%のカレーです。");
 		menuList.add(menu);
 
 		//「ポークカレー」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
-		menu = new HashMap<String, Object>();
+		menu = new HashMap<>();
 		menu.put("name", "ポークカレー");
 		menu.put("price", 420);
 		menu.put("desc", "特選スパイスをきかせた国産ポーク100%のカレーです。");
 		menuList.add(menu);
 
 		//以下データ登録の繰り返し。
-		menu = new HashMap<String, Object>();
+		menu = new HashMap<>();
 		menu.put("name", "ハンバーグカレー");
 		menu.put("price", 620);
 		menu.put("desc", "特選スパイスをきかせたカレーに手ごねハンバーグをトッピングです。");
 		menuList.add(menu);
 
-		menu = new HashMap<String, Object>();
+		menu = new HashMap<>();
 		menu.put("name", "チーズカレー");
 		menu.put("price", 560);
 		menu.put("desc", "特選スパイスをきかせたカレーにとろけるチーズをトッピングです。");
 		menuList.add(menu);
 
-		menu = new HashMap<String, Object>();
+		menu = new HashMap<>();
 		menu.put("name", "カツカレー");
 		menu.put("price", 760);
 		menu.put("desc", "特選スパイスをきかせたカレーに国産ロースカツをトッピングです。");
 		menuList.add(menu);
 
-		menu = new HashMap<String, Object>();
+		menu = new HashMap<>();
 		menu.put("name", "ビーフカツカレー");
 		menu.put("price", 880);
 		menu.put("desc", "特選スパイスをきかせたカレーに国産ビーフカツをトッピングです。");
 		menuList.add(menu);
 
-		menu = new HashMap<String, Object>();
+		menu = new HashMap<>();
 		menu.put("name", "からあげカレー");
 		menu.put("price", 540);
 		menu.put("desc", "特選スパイスをきかせたカレーに若鳥のから揚げをトッピングです。");
