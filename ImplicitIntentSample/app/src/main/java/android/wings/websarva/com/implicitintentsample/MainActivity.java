fa35f61bfa6acc2eba707dafbc5c7c296d8afdaf
Shinzo SAITO <architshin@websarva.com>
2020-11-13 20:39:58 +0900

    各コメントを記述。

--- ImplicitIntentSample/app/src/main/java/android/wings/websarva/com/implicitintentsample/MainActivity.java
+++ ImplicitIntentSample/app/src/main/java/android/wings/websarva/com/implicitintentsample/MainActivity.java
@@ -43,9 +43,17 @@ public class MainActivity extends AppCompatActivity {
 	 * 経度フィールド
 	 */
 	private double _longitude = 0;
-
+	/**
+	 * FusedLocationProviderClientオブジェクトフィールド。
+	 */
 	private FusedLocationProviderClient _fusedLocationClient;
+	/**
+	 * LocationRequestオブジェクトフィールド。
+	 */
 	private LocationRequest _locationRequest;
+	/**
+	 * 位置情報が変更された時の処理を行うコールバックオブジェクトフィールド。
+	 */
 	private OnUpdateLocation _onUpdateLocation;
 
 	@Override
@@ -53,11 +61,17 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		// FusedLocationProviderClientオブジェクトを取得。
 		_fusedLocationClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+		// LocationRequestオブジェクトを生成。
 		_locationRequest = LocationRequest.create();
-		_locationRequest.setInterval(1000);
-		_locationRequest.setFastestInterval(500);
+		// 位置情報の更新間隔を設定。
+		_locationRequest.setInterval(5000);
+		// 位置情報の最短更新間隔を設定。
+		_locationRequest.setFastestInterval(1000);
+		// 位置情報の取得精度を設定。
 		_locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+		// 位置情報が変更された時の処理を行うコールバックオブジェクトを生成。
 		_onUpdateLocation = new OnUpdateLocation();
 	}
 
@@ -65,27 +79,27 @@ public class MainActivity extends AppCompatActivity {
 	protected void onResume() {
 		super.onResume();
 
-		//ACCESS_FINE_LOCATIONの許可が下りていないなら…
+		// ACCESS_FINE_LOCATIONの許可が下りていないなら…
 		if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-			//ACCESS_FINE_LOCATIONの許可を求めるダイアログを表示。その際、リクエストコードを1000に設定。
+			// ACCESS_FINE_LOCATIONの許可を求めるダイアログを表示。その際、リクエストコードを1000に設定。
 			String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION};
 			ActivityCompat.requestPermissions(MainActivity.this, permissions, 1000);
-			//onCreate()メソッドを終了。
+			// onCreate()メソッドを終了。
 			return;
 		}
-		//位置情報の追跡を開始。
+		// 位置情報の追跡を開始。
 		_fusedLocationClient.requestLocationUpdates(_locationRequest, _onUpdateLocation, Looper.getMainLooper());
 	}
 
 	@Override
 	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-		//ACCESS_FINE_LOCATIONに対するパーミションダイアログでかつ許可を選択したなら…
+		// ACCESS_FINE_LOCATIONに対するパーミションダイアログでかつ許可を選択したなら…
 		if(requestCode == 1000 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-			//再度ACCESS_FINE_LOCATIONの許可が下りていないかどうかのチェックをし、降りていないなら処理を中止。
+			// 再度ACCESS_FINE_LOCATIONの許可が下りていないかどうかのチェックをし、降りていないなら処理を中止。
 			if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 				return;
 			}
-			//位置情報の追跡を開始。
+			// 位置情報の追跡を開始。
 			_fusedLocationClient.requestLocationUpdates(_locationRequest, _onUpdateLocation, Looper.getMainLooper());
 		}
 	}
@@ -94,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
 	protected void onPause() {
 		super.onPause();
 
-		//位置情報の追跡を停止。
+		// 位置情報の追跡を停止。
 		_fusedLocationClient.removeLocationUpdates(_onUpdateLocation);
 	}
 
@@ -102,20 +116,20 @@ public class MainActivity extends AppCompatActivity {
 	 * 地図検索ボタンがタップされたときの処理メソッド。
 	 */
 	public void onMapSearchButtonClick(View view) {
-		//入力欄に入力されたキーワード文字列を取得。
+		// 入力欄に入力されたキーワード文字列を取得。
 		EditText etSearchWord = findViewById(R.id.etSearchWord);
 		String searchWord = etSearchWord.getText().toString();
 
 		try {
-			//入力されたキーワードをURLエンコード。
+			// 入力されたキーワードをURLエンコード。
 			searchWord = URLEncoder.encode(searchWord, "UTF-8");
-			//マップアプリと連携するURI文字列を生成。
+			// マップアプリと連携するURI文字列を生成。
 			String uriStr = "geo:0,0?q=" + searchWord;
-			//URI文字列からURIオブジェクトを生成。
+			// URI文字列からURIオブジェクトを生成。
 			Uri uri = Uri.parse(uriStr);
-			//Intentオブジェクトを生成。
+			// Intentオブジェクトを生成。
 			Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-			//アクティビティを起動。
+			// アクティビティを起動。
 			startActivity(intent);
 		}
 		catch(UnsupportedEncodingException ex) {
@@ -127,30 +141,34 @@ public class MainActivity extends AppCompatActivity {
 	 * 現在地の地図表示ボタンがタップされたときの処理メソッド。
 	 */
 	public void onMapShowCurrentButtonClick(View view) {
-		//フィールドの緯度と経度の値をもとにマップアプリと連携するURI文字列を生成。
+		// フィールドの緯度と経度の値をもとにマップアプリと連携するURI文字列を生成。
 		String uriStr = "geo:" + _latitude + "," + _longitude;
-		//URI文字列からURIオブジェクトを生成。
+		// URI文字列からURIオブジェクトを生成。
 		Uri uri = Uri.parse(uriStr);
-		//Intentオブジェクトを生成。
+		// Intentオブジェクトを生成。
 		Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-		//アクティビティを起動。
+		// アクティビティを起動。
 		startActivity(intent);
 	}
 
+	/**
+	 * 位置情報が変更された時の処理を行うコールバッククラス。
+	 */
 	private class OnUpdateLocation extends LocationCallback {
 		@Override
 		public void onLocationResult(LocationResult locationResult) {
 			if(locationResult != null) {
+				// 直近の位置情報を取得。
 				Location location = locationResult.getLastLocation();
 				if(location != null) {
-					//locationオブジェクトから緯度を取得。
+					// locationオブジェクトから緯度を取得。
 					_latitude = location.getLatitude();
-					//locationオブジェクトから経度を取得。
+					// locationオブジェクトから経度を取得。
 					_longitude = location.getLongitude();
-					//取得した緯度をTextViewに表示。
+					// 取得した緯度をTextViewに表示。
 					TextView tvLatitude = findViewById(R.id.tvLatitude);
 					tvLatitude.setText(Double.toString(_latitude));
-					//取得した経度をTextViewに表示。
+					// 取得した経度をTextViewに表示。
 					TextView tvLongitude = findViewById(R.id.tvLongitude);
 					tvLongitude.setText(Double.toString(_longitude));
 				}
