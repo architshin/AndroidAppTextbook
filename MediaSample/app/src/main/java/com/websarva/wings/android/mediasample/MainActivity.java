87d14e25df94af660d6bc1f8bc4c1b85d7dea3c9
Shinzo SAITO <architshin@websarva.com>
2020-11-25 15:22:26 +0900

    第12章「メディアサンプル」を第2版に合わせて改変。 (#38)
    * 12.1節まで終了。
    
    * 一通り完成。

--- MediaSample/app/src/main/java/com/websarva/wings/android/mediasample/MainActivity.java
+++ MediaSample/app/src/main/java/com/websarva/wings/android/mediasample/MainActivity.java
@@ -1,13 +1,16 @@
 package com.websarva.wings.android.mediasample;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
-import android.widget.Switch;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 import java.io.IOException;
 
@@ -20,67 +23,45 @@ import java.io.IOException;
  *
  * @author Shinzo SAITO
  */
-public class MediaControlActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 	/**
 	 * メディアプレーヤーフィールド。
 	 */
 	private MediaPlayer _player;
 
-	/**
-	 * 再生・一時停止ボタンフィールド。
-	 */
-	private Button _btPlay;
-
-	/**
-	 * 戻るボタンフィールド。
-	 */
-	private Button _btBack;
-
-	/**
-	 * 進むボタンフィールド。
-	 */
-	private Button _btForward;
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_media_control);
+		setContentView(R.layout.activity_main);
 
-		//フィールドの各ボタンを取得。
-		_btPlay = findViewById(R.id.btPlay);
-		_btBack = findViewById(R.id.btBack);
-		_btForward = findViewById(R.id.btForward);
-
-		//フィールドのメディアプレーヤーオブジェクトを生成。
+		// フィールドのメディアプレーヤーオブジェクトを生成。
 		_player = new MediaPlayer();
-		//音声ファイルのURI文字列を作成。
+		// 音声ファイルのURI文字列を作成。
 		String mediaFileUriStr = "android.resource://" + getPackageName() + "/" + R.raw.mountain_stream;
-		//音声ファイルのURI文字列を元にURIオブジェクトを生成。
+		// 音声ファイルのURI文字列を元にURIオブジェクトを生成。
 		Uri mediaFileUri = Uri.parse(mediaFileUriStr);
 		try {
-			//メディアプレーヤーに音声ファイルを指定。
-			_player.setDataSource(MediaControlActivity.this, mediaFileUri);
-			//非同期でのメディア再生準備が完了した際のリスナを設定。
+			// メディアプレーヤーに音声ファイルを指定。
+			_player.setDataSource(MainActivity.this, mediaFileUri);
+			// 非同期でのメディア再生準備が完了した際のリスナを設定。
 			_player.setOnPreparedListener(new PlayerPreparedListener());
-			//メディア再生が終了した際のリスナを設定。
+			// メディア再生が終了した際のリスナを設定。
 			_player.setOnCompletionListener(new PlayerCompletionListener());
-			//非同期でメディア再生を準備。
+			// 非同期でメディア再生を準備。
 			_player.prepareAsync();
 		}
-		catch(IOException e) {
-			e.printStackTrace();
+		catch(IOException ex) {
+			Log.e("MediaSample", "メディアプレーヤー準備時の例外発生", ex);
 		}
 
 		//スイッチを取得。
-		Switch loopSwitch = findViewById(R.id.swLoop);
+		SwitchMaterial loopSwitch = findViewById(R.id.swLoop);
 		//スイッチにリスナを設定。
 		loopSwitch.setOnCheckedChangeListener(new LoopSwitchChangedListener());
 	}
 
 	@Override
 	protected void onDestroy() {
-		//親クラスのメソッド呼び出し。
-		super.onDestroy();
 		//プレーヤーが再生中なら…
 		if(_player.isPlaying()) {
 			//プレーヤーを停止。
@@ -90,6 +71,8 @@ public class MediaControlActivity extends AppCompatActivity {
 		_player.release();
 		//プレーヤー用フィールドをnullに。
 		_player = null;
+		//親クラスのメソッド呼び出し。
+		super.onDestroy();
 	}
 
 	/**
@@ -98,19 +81,21 @@ public class MediaControlActivity extends AppCompatActivity {
 	 * @param view 画面部品
 	 */
 	public void onPlayButtonClick(View view) {
-		//プレーヤーが再生中だったら…
+		// 再生ボタンを取得。
+		Button btPlay = findViewById(R.id.btPlay);
+		// プレーヤーが再生中だったら…
 		if(_player.isPlaying()) {
-			//プレーヤーを一時停止。
+			// プレーヤーを一時停止。
 			_player.pause();
-			//再生ボタンのラベルを「再生」に設定。
-			_btPlay.setText(R.string.bt_play_play);
+			// 再生ボタンのラベルを「再生」に設定。
+			btPlay.setText(R.string.bt_play_play);
 		}
-		//プレーヤーが再生中じゃなかったら…
+		// プレーヤーが再生中じゃなかったら…
 		else {
-			//プレーヤーを再生。
+			// プレーヤーを再生。
 			_player.start();
-			//再生ボタンのラベルを「一時停止」に設定。
-			_btPlay.setText(R.string.bt_play_pause);
+			// 再生ボタンのラベルを「一時停止」に設定。
+			btPlay.setText(R.string.bt_play_pause);
 		}
 	}
 
@@ -145,13 +130,15 @@ public class MediaControlActivity extends AppCompatActivity {
 	 * プレーヤーの再生準備が整った時のリスナクラス。
 	 */
 	private class PlayerPreparedListener implements MediaPlayer.OnPreparedListener {
-
 		@Override
 		public void onPrepared(MediaPlayer mp) {
-			//各ボタンをタップ可能に設定。
-			_btPlay.setEnabled(true);
-			_btBack.setEnabled(true);
-			_btForward.setEnabled(true);
+			// 各ボタンをタップ可能に設定。
+			Button btPlay = findViewById(R.id.btPlay);
+			btPlay.setEnabled(true);
+			Button btBack = findViewById(R.id.btBack);
+			btBack.setEnabled(true);
+			Button btForward = findViewById(R.id.btForward);
+			btForward.setEnabled(true);
 		}
 	}
 
@@ -159,13 +146,13 @@ public class MediaControlActivity extends AppCompatActivity {
 	 * 再生が終了したときのリスナクラス。
 	 */
 	private class PlayerCompletionListener implements MediaPlayer.OnCompletionListener {
-
 		@Override
 		public void onCompletion(MediaPlayer mp) {
 			//ループ設定がされていないならば…
 			if(!_player.isLooping()) {
-				//再生ボタンのラベルを「再生」に設定。
-				_btPlay.setText(R.string.bt_play_play);
+				// 再生ボタンのラベルを「再生」に設定。
+				Button btPlay = findViewById(R.id.btPlay);
+				btPlay.setText(R.string.bt_play_play);
 			}
 		}
 	}
@@ -174,7 +161,6 @@ public class MediaControlActivity extends AppCompatActivity {
 	 * リピート再生スイッチの切替時のリスナクラス。
 	 */
 	private class LoopSwitchChangedListener implements CompoundButton.OnCheckedChangeListener {
-
 		@Override
 		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 			//ループするかどうかを設定。
