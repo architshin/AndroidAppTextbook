04087d3dd48d7609ef378a08f907bac802880767
Shinzo SAITO <architshin@websarva.com>
2020-12-07 13:35:14 +0900

    第8章「オプションメニューサンプル」の追加。 (#42)

---
+++ OptionsMenuSample/app/src/main/java/com/websarva/wings/android/optionsmenusample/MainActivity.java
@@ -0,0 +1,22 @@
+package com.websarva.wings.android.optionsmenusample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.options_menu_sample, menu);
+		return true;
+	}
+}
