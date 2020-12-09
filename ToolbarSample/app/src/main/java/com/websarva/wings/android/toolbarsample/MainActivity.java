e90372981c61dd6553cbbb2c709abc11f7bd5850
Shinzo SAITO <architshin@websarva.com>
2020-12-09 14:14:41 +0900

    第16章「ツールバーサンプル」を第2版に合わせて改変。

--- ToolbarSample/app/src/main/java/com/websarva/wings/android/toolbarsample/MainActivity.java
+++ ToolbarSample/app/src/main/java/com/websarva/wings/android/toolbarsample/MainActivity.java
@@ -1,25 +1,25 @@
 package com.websarva.wings.android.toolbarsample;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
 /**
  * 『Androidアプリ開発の教科書』
- * 第15章
+ * 第16章
  * ツールバーサンプル
  *
  * アクティビティクラス。
  *
  * @author Shinzo SAITO
  */
-public class ScrollArticleActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_scroll_article);
+		setContentView(R.layout.activity_main);
 
 		//Toolbarを取得。
 		Toolbar toolbar = findViewById(R.id.toolbar);
