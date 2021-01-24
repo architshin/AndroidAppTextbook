92ef288c9eb1f2fd0285bd442fff811da9ef7541
Shinzo SAITO <architshin@websarva.com>
2021-01-25 01:02:22 +0900

    「第4章 イベントとリスナサンプル」を初校に合わせて変更。

--- HelloSample/app/src/main/java/com/websarva/wings/android/hellosample/MainActivity.java
+++ HelloSample/app/src/main/java/com/websarva/wings/android/hellosample/MainActivity.java
@@ -23,16 +23,16 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		//表示ボタンであるButtonオブジェクトを取得。
+		// 表示ボタンであるButtonオブジェクトを取得。
 		Button btClick = findViewById(R.id.btClick);
-		//リスナクラスのインスタンスを生成。
+		// リスナクラスのインスタンスを生成。
 		HelloListener listener = new HelloListener();
-		//表示ボタンにリスナを設定。
+		// 表示ボタンにリスナを設定。
 		btClick.setOnClickListener(listener);
 
-		//クリアボタンであるButtonオブジェクトを取得。
+		// クリアボタンであるButtonオブジェクトを取得。
 		Button btClear = findViewById(R.id.btClear);
-		//クリアボタンにリスナを設定。
+		// クリアボタンにリスナを設定。
 		btClear.setOnClickListener(listener);
 	}
 
@@ -42,27 +42,27 @@ public class MainActivity extends AppCompatActivity {
 	private class HelloListener implements View.OnClickListener {
 		@Override
 		public void onClick(View view) {
-			//名前入力欄であるEditTextオブジェクトを取得。
+			// 名前入力欄であるEditTextオブジェクトを取得。
 			EditText input = findViewById(R.id.etName);
-			//メッセージを表示するTextViewオブジェクトを取得。
+			// メッセージを表示するTextViewオブジェクトを取得。
 			TextView output = findViewById(R.id.tvOutput);
 
-			//タップされた画面部品のidのR値を取得。
+			// タップされた画面部品のidのR値を取得。
 			int id = view.getId();
-			//idのR値に応じて処理を分岐。
+			// idのR値に応じて処理を分岐。
 			switch(id) {
-				//表示ボタンの場合…
+				// 表示ボタンの場合…
 				case R.id.btClick:
-					//入力された名前文字列を取得。
+					// 入力された名前文字列を取得。
 					String inputStr = input.getText().toString();
-					//メッセージを表示。
+					// メッセージを表示。
 					output.setText(inputStr + "さん、こんにちは!");
 					break;
-				//クリアボタンの場合…
+				// クリアボタンの場合…
 				case R.id.btClear:
-					//名前入力欄を空文字に設定。
+					// 名前入力欄を空文字に設定。
 					input.setText("");
-					//メッセージ表示欄を空文字に設定。
+					// メッセージ表示欄を空文字に設定。
 					output.setText("");
 					break;
 			}
