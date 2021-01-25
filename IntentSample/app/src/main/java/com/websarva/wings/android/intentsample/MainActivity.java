fbe19cd7b87df7e77300533d3ba9134b42e854b5
Shinzo SAITO <architshin@websarva.com>
2021-01-25 12:20:36 +0900

    「第7章 画面遷移サンプル」を初校に合わせて変更。

--- IntentSample/app/src/main/java/com/websarva/wings/android/intentsample/MainActivity.java
+++ IntentSample/app/src/main/java/com/websarva/wings/android/intentsample/MainActivity.java
@@ -29,24 +29,24 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		//画面部品ListViewを取得
+		// 画面部品ListViewを取得
 		ListView lvMenu = findViewById(R.id.lvMenu);
-		//SimpleAdapterで使用するListオブジェクトを用意。
+		// SimpleAdapterで使用するListオブジェクトを用意。
 		List<Map<String, String>> menuList = new ArrayList<>();
 
-		//「から揚げ定食」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
+		// 「から揚げ定食」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
 		Map<String, String> menu = new HashMap<>();
 		menu.put("name", "から揚げ定食");
 		menu.put("price", "800円");
 		menuList.add(menu);
 
-		//「ハンバーグ定食」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
+		// 「ハンバーグ定食」のデータを格納するMapオブジェクトの用意とmenuListへのデータ登録。
 		menu = new HashMap<>();
 		menu.put("name", "ハンバーグ定食");
 		menu.put("price", "850円");
 		menuList.add(menu);
 
-		//以下データ登録の繰り返し。
+		// 以下データ登録の繰り返し。
 		menu = new HashMap<>();
 		menu.put("name", "生姜焼き定食");
 		menu.put("price", "850円");
@@ -83,6 +83,21 @@ public class MainActivity extends AppCompatActivity {
 		menuList.add(menu);
 
 		menu = new HashMap<>();
+		menu.put("name", "回鍋肉定食");
+		menu.put("price", "750円");
+		menuList.add(menu);
+
+		menu = new HashMap<>();
+		menu.put("name", "麻婆豆腐定食");
+		menu.put("price", "800円");
+		menuList.add(menu);
+
+		menu = new HashMap<>();
+		menu.put("name", "青椒肉絲定食");
+		menu.put("price", "900円");
+		menuList.add(menu);
+
+		menu = new HashMap<>();
 		menu.put("name", "焼き魚定食");
 		menu.put("price", "850円");
 		menuList.add(menu);
@@ -92,16 +107,16 @@ public class MainActivity extends AppCompatActivity {
 		menu.put("price", "950円");
 		menuList.add(menu);
 
-		//SimpleAdapter第4引数from用データの用意。
+		// SimpleAdapter第4引数from用データの用意。
 		String[] from = {"name", "price"};
-		//SimpleAdapter第5引数to用データの用意。
+		// SimpleAdapter第5引数to用データの用意。
 		int[] to = {android.R.id.text1, android.R.id.text2};
 		//SimpleAdapterを生成。
 		SimpleAdapter adapter = new SimpleAdapter(MainActivity.this, menuList, android.R.layout.simple_list_item_2, from, to);
-		//アダプタの登録。
+		// アダプタの登録。
 		lvMenu.setAdapter(adapter);
 
-		//リストタップのリスナクラス登録。
+		// リストタップのリスナクラス登録。
 		lvMenu.setOnItemClickListener(new ListItemClickListener());
 	}
 
@@ -111,18 +126,18 @@ public class MainActivity extends AppCompatActivity {
 	private class ListItemClickListener implements AdapterView.OnItemClickListener {
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-			//タップされた行のデータを取得。SimpleAdapterでは1行分のデータはMap型!
+			// タップされた行のデータを取得。SimpleAdapterでは1行分のデータはMap型!
 			Map<String, String> item = (Map<String, String>) parent.getItemAtPosition(position);
-			//定食名と金額を取得。
+			// 定食名と金額を取得。
 			String menuName = item.get("name");
 			String menuPrice = item.get("price");
 
-			//インテントオブジェクトを生成。
+			// インテントオブジェクトを生成。
 			Intent intent = new Intent(MainActivity.this, MenuThanksActivity.class);
-			//第2画面に送るデータを格納。
+			// 第2画面に送るデータを格納。
 			intent.putExtra("menuName", menuName);
 			intent.putExtra("menuPrice", menuPrice);
-			//第2画面の起動。
+			// 第2画面の起動。
 			startActivity(intent);
 		}
 	}
